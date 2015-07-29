aa0d6cf468510ccf92defcdeb67fca596d93636e
Alexei Colin <ac@alexeicolin.com>
2015-07-29 10:42:29 -0400

    wisp-base: uart: mark ISR-main flags volatile

--- CCS/wisp-base/wired/uart.c
+++ CCS/wisp-base/wired/uart.c
@@ -5,22 +5,25 @@
  * @brief UART module for transmitting/receiving data using the USCI_A0 peripheral
  */
 
+#include <msp430.h>
 #include "uart.h"
 #include "../globals.h"
 
 /**
  * State variables for the UART module
  */
-struct {
-    uint8_t isTxBusy; // Is the module currently in the middle of a transmit operation?
-    uint8_t* txPtr; // Pointer to the next byte to be transmitted
-    uint16_t txBytesRemaining; // Number of bytes left to send
+typedef struct {
+    volatile uint8_t isTxBusy; // Is the module currently in the middle of a transmit operation?
+    volatile uint8_t* txPtr; // Pointer to the next byte to be transmitted
+    volatile uint16_t txBytesRemaining; // Number of bytes left to send
 
-    uint8_t isRxBusy; // Is the module currently in the middle of a receive operation?
-    uint8_t* rxPtr; // Pointer to the next byte to be received
-    uint16_t rxBytesRemaining; // Maximum number of bytes left to receive
-    uint8_t rxTermChar; // Stop receiving on this char.
-} UART_SM;
+    volatile uint8_t isRxBusy; // Is the module currently in the middle of a receive operation?
+    volatile uint8_t* rxPtr; // Pointer to the next byte to be received
+    volatile uint16_t rxBytesRemaining; // Maximum number of bytes left to receive
+    volatile uint8_t rxTermChar; // Stop receiving on this char.
+} uart_sm_t;
+
+static volatile uart_sm_t UART_SM;
 
 /**
  * Configure the eUSCI_A0 module in UART mode and prepare for UART transmission.
